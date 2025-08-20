fa62311dc5ea71be904e55db9fc758db869a0c42
Weekaaa <alyalwakeel9@gmail.com>
2025-08-20 13:41:35 +0300

    Created and populated the app bar

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(body: const HomeViewBody());
   }
 }
